ec0ce952fa2dc59dbc656e3de35c887ec0a24366
Emily Samp <emily.samp@icloud.com>
2024-12-02 10:21:46 -0600

    Day 2: Part 2

--- 02.rb
+++ 02.rb
@@ -12,17 +12,19 @@ module Day02
       reports.count(&:safe?)
     end
 
+    sig { params(input: T::Array[String]).returns(Integer) }
     def part_two(input)
-      raise NotImplementedError
+      reports = process(input, problem_dampener: true)
+      reports.count(&:safe?)
     end
 
     private
 
-    sig { params(input: T::Array[String]).returns(T::Array[Report]) }
-    def process(input)
+    sig { params(input: T::Array[String], problem_dampener: T::Boolean).returns(T::Array[Report]) }
+    def process(input, problem_dampener: false)
       input.map do |line|
         levels = line.split.map(&:to_i)
-        Report.new(levels)
+        Report.new(levels, problem_dampener: problem_dampener)
       end
     end
   end
@@ -30,10 +32,24 @@ module Day02
   class Report
     extend T::Sig
 
-    sig { params(input: T::Array[Integer]).void }
-    def initialize(input)
+    sig { params(input: T::Array[Integer], problem_dampener: T::Boolean).void }
+    def initialize(input, problem_dampener: false)
       @levels = input
-      @safe = T.let((all_increasing? || all_decreasing?) && close_levels?, T::Boolean)
+
+      if problem_dampener
+        modified_reports = []
+
+        (0...@levels.length).each do |idx|
+          new_levels = @levels.dup
+          new_levels.delete_at(idx)
+
+          modified_reports << Report.new(new_levels)
+        end
+
+        @safe = T.let(modified_reports.any?(&:safe?), T::Boolean)
+      else
+        @safe = T.let((all_increasing? || all_decreasing?) && close_levels?, T::Boolean)
+      end
     end
 
     sig { returns(T::Boolean) }
