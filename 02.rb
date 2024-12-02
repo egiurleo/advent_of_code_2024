bd4fd25668b4b1af1a066a05513ca937bd2188ef
Emily Samp <emily.samp@icloud.com>
2024-12-02 11:22:57 -0600

    Refactor Day 2

--- 02.rb
+++ 02.rb
@@ -8,48 +8,24 @@ module Day02
 
     sig { params(input: T::Array[String]).returns(Integer) }
     def part_one(input)
-      reports = process(input)
+      reports = input.map { |line| Report.new(line.split.map(&:to_i)) }
       reports.count(&:safe?)
     end
 
     sig { params(input: T::Array[String]).returns(Integer) }
     def part_two(input)
-      reports = process(input, problem_dampener: true)
+      reports = input.map { |line| DampenedReport.new(line.split.map(&:to_i)) }
       reports.count(&:safe?)
-    end
-
-    private
-
-    sig { params(input: T::Array[String], problem_dampener: T::Boolean).returns(T::Array[Report]) }
-    def process(input, problem_dampener: false)
-      input.map do |line|
-        levels = line.split.map(&:to_i)
-        Report.new(levels, problem_dampener: problem_dampener)
-      end
     end
   end
 
   class Report
     extend T::Sig
 
-    sig { params(input: T::Array[Integer], problem_dampener: T::Boolean).void }
-    def initialize(input, problem_dampener: false)
+    sig { params(input: T::Array[Integer]).void }
+    def initialize(input)
       @levels = input
-
-      if problem_dampener
-        modified_reports = []
-
-        (0...@levels.length).each do |idx|
-          new_levels = @levels.dup
-          new_levels.delete_at(idx)
-
-          modified_reports << Report.new(new_levels)
-        end
-
-        @safe = T.let(modified_reports.any?(&:safe?), T::Boolean)
-      else
-        @safe = T.let((all_increasing? || all_decreasing?) && close_levels?, T::Boolean)
-      end
+      @safe = T.let((all_increasing? || all_decreasing?) && close_levels?, T::Boolean)
     end
 
     sig { returns(T::Boolean) }
@@ -76,6 +52,27 @@ module Day02
 
         diff = (level1 - level2).abs
         diff >= 1 && diff <= 3
+      end
+    end
+  end
+
+  class DampenedReport < Report
+    sig { params(input: T::Array[Integer]).void }
+    def initialize(input)
+      super
+
+      @safe = T.let(modified_reports.any?(&:safe?), T::Boolean)
+    end
+
+    private
+
+    sig { returns(T::Array[Report]) }
+    def modified_reports
+      (0...@levels.length).map do |idx|
+        new_levels = @levels.dup
+        new_levels.delete_at(idx)
+
+        Report.new(new_levels)
       end
     end
   end
