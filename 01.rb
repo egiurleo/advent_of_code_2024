3a8225a8bae115e5c845590f6d0e53695af7a6b1
Emily Samp <emily.samp@icloud.com>
2024-12-02 11:25:23 -0600

    Refactor Day 1

--- 01.rb
+++ 01.rb
@@ -10,28 +10,18 @@ module Day01
     sig { params(input: T::Array[String]).returns(Integer) }
     def part_one(input)
       list1, list2 = process(input)
-
-      list1 = list1.sorted
-      list2 = list2.sorted
-
-      (0...list1.length).map { |idx| (list1.get(idx) - list2.get(idx)).abs }.reduce(:+)
+      list1.total_distance(list2)
     end
 
     sig { params(input: T::Array[String]).returns(Integer) }
     def part_two(input)
       list1, list2 = process(input)
-
-      (0...list1.length).map do |idx|
-        val = list1.get(idx)
-        count = list2.count(val)
-
-        val * count
-      end.reduce(:+)
+      list1.similarity_score(list2)
     end
 
     private
 
-    sig { params(input: T::Array[String]).returns([List, List]) }
+    sig { params(input: T::Array[String]).returns([SortedList, SortedList]) }
     def process(input)
       list1 = T.let([], T::Array[Integer])
       list2 = T.let([], T::Array[Integer])
@@ -42,16 +32,16 @@ module Day01
         list2 << T.must(int2)
       end
 
-      [List.new(list1), List.new(list2)]
+      [SortedList.new(list1), SortedList.new(list2)]
     end
   end
 
-  class List
+  class SortedList
     extend T::Sig
 
     sig { params(list: T::Array[Integer]).void }
     def initialize(list)
-      @list = list
+      @list = T.let(list.sort, T::Array[Integer])
       @counts = T.let(
         list.each_with_object(Hash.new(0)) do |elem, h|
           h[elem] += 1
@@ -74,14 +64,18 @@ module Day01
       @counts[val] || 0
     end
 
-    sig { returns(Integer) }
-    def length
-      @list.length
+    sig { params(other_list: SortedList).returns(Integer) }
+    def total_distance(other_list)
+      @list.map.with_index do |elem, idx|
+        (elem - other_list.get(idx)).abs
+      end.reduce(:+)
     end
 
-    sig { returns(List) }
-    def sorted
-      self.class.new(@list.sort)
+    sig { params(other_list: SortedList).returns(Integer) }
+    def similarity_score(other_list)
+      @list.map do |elem|
+        elem * other_list.count(elem)
+      end.reduce(:+)
     end
   end
 end
