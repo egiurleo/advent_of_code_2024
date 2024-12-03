8a4cb1407b2e0d40c8c58c64aee620deee808664
Emily Samp <emily.samp@icloud.com>
2024-12-03 10:33:22 -0600

    Refactor Day 3 Part 1

--- 03.rb
+++ 03.rb
@@ -9,42 +9,110 @@ module Day03
 
     sig { params(input: T::Array[String]).returns(Integer) }
     def part_one(input)
-      input.map do |line|
-        extract_mult_operations(line)
-      end.flatten.map(&:result).reduce(:+)
+      Program.new(input.join).run.sum
     end
 
     sig { params(input: T::Array[String]).returns(Integer) }
     def part_two(input)
       raise NotImplementedError
     end
+  end
 
-    private
+  class Program
+    extend T::Sig
 
-    sig { params(input: String).returns(T::Array[Multiply]) }
-    def extract_mult_operations(input)
-      input.scan(Multiply::FORMAT).map do |operation|
-        operation = T.cast(operation, [String, String])
-        val1, val2 = operation
+    sig { params(input: String).void }
+    def initialize(input)
+      @operations = T.let([], T::Array[Operation])
+      @program_state = T.let(ProgramState.new, ProgramState)
 
-        Multiply.new(val1.to_i, val2.to_i)
-      end.compact
+      input.scan(Multiply::FORMAT).map do |op_string|
+        op_string = T.cast(op_string, String)
+
+        op_string = op_string.delete_prefix('mul(').delete_suffix(')')
+        val1, val2 = op_string.split(',')
+
+        raise ArgumentError if val1.nil? || val2.nil?
+
+        @operations << Multiply.new(val1.to_i, val2.to_i)
+      end
+    end
+
+    sig { returns(Integer) }
+    def sum
+      @program_state.sum
+    end
+
+    sig { returns(Program) }
+    def run
+      @operations.each { |op| op.perform(@program_state) }
+      self
     end
   end
 
-  class Multiply
+  class ProgramState
     extend T::Sig
 
-    FORMAT = T.let(/mul\((\d{1,3}),(\d{1,3})\)/, Regexp)
+    sig { returns(Integer) }
+    attr_reader :sum
+
+    sig { void }
+    def initialize
+      @sum = T.let(0, Integer)
+      @mult_allowed = T.let(true, T::Boolean)
+    end
+
+    sig { params(val: Integer).void }
+    def add(val)
+      @sum += val
+    end
+
+    sig { void }
+    def allow_mult
+      @mult_allowed = true
+    end
+
+    sig { void }
+    def disallow_mult
+      @mult_allowed = false
+    end
+
+    sig { returns(T::Boolean) }
+    def mult_allowed?
+      @mult_allowed
+    end
+  end
+
+  class Operation
+    extend T::Sig
+    extend T::Helpers
+
+    abstract!
+
+    sig { abstract.params(state: ProgramState).void }
+    def perform(state); end
+  end
+
+  class Multiply < Operation
+    extend T::Sig
+
+    FORMAT = T.let(/mul\(\d{1,3},\d{1,3}\)/, Regexp)
 
     sig { returns(Integer) }
     attr_reader :result
 
     sig { params(val1: Integer, val2: Integer).void }
     def initialize(val1, val2)
+      super()
+
       @val1 = T.let(val1, Integer)
       @val2 = T.let(val2, Integer)
       @result = T.let(@val1 * @val2, Integer)
+    end
+
+    sig { override.params(state: ProgramState).void }
+    def perform(state)
+      state.add(@result)
     end
   end
 end
